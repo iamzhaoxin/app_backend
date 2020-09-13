b7cb58ba5b9f11ab21e1bf72a1ebd0d5eb051928
Zhaoxin <zhaoxin130120@126.com>
2020-09-13 10:35:23 +0800

    add testapp

--- app_backend/urls.py
+++ app_backend/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+
+import testapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('testapp/',include('testapp.urls')),
 ]
---
+++ testapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from testapp.views import hello
+
+urlpatterns = [
+    path('hello/',hello),
+]
