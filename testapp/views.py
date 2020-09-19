6746b8790529c939bae962ac434dbca6a10c43ab
Zhaoxin <zhaoxin130120@126.com>
2020-09-19 11:11:10 +0800

    !remove CSRF settings.py 注释第47行取消了CSRF安全验证 --- 修改了test，增加了对request的response

--- testapp/views.py
+++ testapp/views.py
@@ -6,5 +6,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello(request):
-    res=['Hi~','iamzhaoxin']
-    return HttpResponse(json.dumps(res))
+    if request.method=="POST":
+        name=request.POST.get('name')
+        res=['Hi~',name]
+    else:
+        res='Hi~,you are using "get" to visit this website'
+    return HttpResponse((res))  #json.dumps(res) : 返回json格式
