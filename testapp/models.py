5d11185160b6bebcc36806bcb0a5404f412619d3
Zhaoxin <zhaoxin130120@126.com>
2020-10-09 20:04:47 +0800

    格式化代码。。。。

--- testapp/models.py
+++ testapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 
 class test(models.Model):
-    name=models.CharField(max_length=20)
-    id=models.IntegerField(primary_key=True) #主键，唯一值
+    name = models.CharField(max_length=20)
+    id = models.IntegerField(primary_key=True)  # 主键，唯一值
 
 # 创建数据库模型类
--- testapp/views.py
+++ testapp/views.py
@@ -9,16 +9,16 @@ from testapp.models import test
 
 
 def hello(request):
-    dtime=datetime.datetime.now().strftime("%d%H%M%S")  #获取当前时间并转化为字符串作为id，因为id自动当选主键，唯一值！
+    dtime = datetime.datetime.now().strftime("%d%H%M%S")  # 获取当前时间并转化为字符串作为id，因为id自动当选主键，唯一值！
 
-    k=test.objects.create(name="zhaoxin",id=dtime)  #插入数据库
+    k = test.objects.create(name="zhaoxin", id=dtime)  # 插入数据库
     if k:
-        dres='数据库添加成功2,id='+dtime
+        dres = '数据库添加成功2,id=' + dtime
     else:
-        dres='数据库添加失败3'
-    if request.method=="POST":
-        name=request.POST.get('name')
-        res=['Hi~',name]
+        dres = '数据库添加失败3'
+    if request.method == "POST":
+        name = request.POST.get('name')
+        res = ['Hi~', name]
     else:
-        res='Hi~,you are using "get" to visit this website'
-    return HttpResponse((dres+res))  #json.dumps(res) : 返回json格式
+        res = 'Hi~,you are using "get" to visit this website'
+    return HttpResponse((dres + res))  # json.dumps(res) : 返回json格式
