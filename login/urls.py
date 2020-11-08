383f67a15e48e4b6570e70d50f807943f3001e95
Zhaoxin <zhaoxin130120@126.com>
2020-11-08 21:52:50 +0800

    增加对昵称、密码、头像的修改功能；针对不同的错误异常添加了检查和错误代码

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from login.views import registered, login
+from login.views import registered, login, change_name, change_password, change_img
 
 urlpatterns = [
-    path('registered/', registered),
-    path('',login),
+    path('registered', registered),
+    path('login',login),
+    path('changename',change_name),
+    path('changepassword',change_password),
+    path('changeimg',change_img),
 ]
--- login/views.py
+++ login/views.py
@@ -8,6 +8,8 @@ from login.models import User
 
 
 def registered(request):
+    if not request.method == "POST":
+        return HttpResponse('-1')  # 未使用POST访问
 
     id = request.POST.get('id')
     name = request.POST.get('name')
@@ -16,21 +18,81 @@ def registered(request):
     identity = request.POST.get('identity')
 
     if User.objects.filter(id__exact=id):
-        return HttpResponse('用户id已存在')
+        return HttpResponse('1')  # 用户id已被注册
     else:
         User.objects.create(id=id, name=name, password=password, image_path=img, identity=identity)
-        return HttpResponse('注册成功')
+        return HttpResponse('0')  # 注册成功
 
 
 def login(request):
+    if not request.method == "POST":
+        return HttpResponse('-1')  # 未使用POST访问
+
     id = request.POST.get('id')
     password = request.POST.get('password')
 
     passw = User.objects.filter(id=id)
 
     if not passw:
-        return HttpResponse('用户不存在')
+        return HttpResponse('1')  # 用户id不存在
     elif password == passw[0].password:
-        return HttpResponse('Yes')
+        return HttpResponse('0')  # 登陆成功
     else:
-        return HttpResponse('No')
+        return HttpResponse('2')  # 密码错误
+
+
+def change_name(request):
+    if not request.method == "POST":
+        return HttpResponse('-1')  # 未使用POST访问
+
+    id = request.POST.get('id')
+    name = request.POST.get('name')
+
+    if not User.objects.filter(id=id):
+        return HttpResponse('1')  # 用户id不存在
+
+    user = User.objects.get(id=id)
+    user.name = name;
+    user.save()
+
+    if User.objects.filter(id=id)[0].name == name:
+        return HttpResponse('0')  # name修改成功
+    else:
+        return HttpResponse('2')  # 查询到name修改失败
+
+
+def change_password(request):
+    if not request.method == "POST":
+        return HttpResponse('-1')  # 未使用POST访问
+
+    id = request.POST.get('id')
+    password = request.POST.get('password')
+
+    if not User.objects.filter(id=id):
+        return HttpResponse('1')  # 用户id不存在
+
+    user = User.objects.get(id=id)
+    user.password = password;
+    user.save()
+
+    if User.objects.filter(id=id)[0].password == password:
+        return HttpResponse('0')  # password修改成功
+    else:
+        return HttpResponse('2')  # 查询到password修改失败
+
+
+def change_img(request):
+    if not request.method == "POST":
+        return HttpResponse('-1')  # 未使用POST访问
+
+    id = request.POST.get('id')
+    img = request.FILES.get('file')
+
+    if not User.objects.filter(id=id):
+        return HttpResponse('1')  # 用户id不存在
+
+    user = User.objects.get(id=id)
+    user.image_path = img;
+    user.save()
+
+    return HttpResponse('0')  # 头像修改成功
