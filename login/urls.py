6de6f7274b3fdaa3bdf5c7e36e721ce37fe6c5ba
Zhaoxin <zhaoxin130120@126.com>
2020-12-11 18:58:19 +0800

    完善接口文档？

--- login/urls.py
+++ login/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from login.views import registered, login, change_name, change_password, change_img
 
+# 前路由：path('login/',include('login.urls')),
 urlpatterns = [
     path('registered', registered),
-    path('login',login),
-    path('changename',change_name),
-    path('changepassword',change_password),
-    path('changeimg',change_img),
+    path('login', login),
+    path('changename', change_name),
+    path('changepassword', change_password),
+    path('changeimg', change_img),
 ]
--- testapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from testapp.views import hello
-
-urlpatterns = [
-    path('hello/', hello),
-]
